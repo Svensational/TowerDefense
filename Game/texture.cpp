5847b4feb5f2f5d6088818bc6f2969f42efc7d60
Svensational <sven.klingel@gmail.com>
2014-02-17 10:11:18 +0100

    implemented a base class for texture classes.

---
+++ Game/texture.cpp
@@ -0,0 +1,74 @@
+#include "texture.h"
+#include "gl_core_4_4.h"
+
+Texture::Texture(Target target) :
+   name(0), target(target)
+{
+   glGenTextures(1, &name);
+}
+
+Texture::~Texture() {
+   glDeleteTextures(1, &name);
+}
+
+void Texture::bind() const {
+   glBindTexture(target, name);
+}
+
+int Texture::getMaxAnisotropyMax() {
+   static int maxAnisotropyMax = getMaxAnisotropyMaxPriv();
+   return maxAnisotropyMax;
+}
+
+int Texture::getMaxAnisotropyMaxPriv() {
+   float maxAnisotropyMax;
+   // GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT = 0x84FF
+   glGetFloatv(0x84FF, &maxAnisotropyMax);
+   return maxAnisotropyMax;
+}
+
+int Texture::getMaxTextureImageUnits() {
+   static int maxTextureImageUnits = getMaxTextureImageUnitsPriv();
+   return maxTextureImageUnits;
+}
+
+int Texture::getMaxTextureImageUnitsPriv() {
+   int maxTextureImageUnits;
+   glGetIntegerv(GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS, &maxTextureImageUnits);
+   return maxTextureImageUnits;
+}
+
+void Texture::setActiveTextureImageUnit(int unit) {
+   glActiveTexture(GL_TEXTURE0 + unit);
+}
+
+void Texture::setBorderColor(float r, float g, float b, float a) const {
+   float color[4] = {r, g, b, a};
+   glTexParameterfv(target, GL_TEXTURE_BORDER_COLOR, color);
+}
+
+void Texture::setMinFilter(Filter filter) const {
+   glTexParameteri(target, GL_TEXTURE_MIN_FILTER, filter);
+}
+
+void Texture::setMinFilter(Filter filter, Filter mipmapFilter) const {
+   int glFilter = GL_NEAREST_MIPMAP_NEAREST;
+   if (filter == LINEAR) glFilter += 1;
+   if (mipmapFilter == LINEAR) glFilter += 2;
+   glTexParameteri(target, GL_TEXTURE_MIN_FILTER, glFilter);
+}
+
+void Texture::setMagFilter(Filter filter) const {
+   glTexParameteri(target, GL_TEXTURE_MAG_FILTER, filter);
+}
+
+void Texture::setMaxAnisotropy(int value) {
+   if (value == 0) value = 1;
+   if (value < 0 || value > getMaxAnisotropyMax()) value = getMaxAnisotropyMax();
+   // GL_TEXTURE_MAX_ANISOTROPY_EXT = 0x84FE
+   glTexParameterf(target, 0x84FE, value);
+}
+
+void Texture::unbind() const {
+   glBindTexture(target, 0);
+}
---
+++ Game/texture.h
@@ -0,0 +1,58 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+class Texture {
+
+public:
+   enum Target {
+      TEXTURE_1D                   = 0x0DE0, // 1.1
+      TEXTURE_2D                   = 0x0DE1, // 1.1
+      TEXTURE_3D                   = 0x806F, // 1.2
+      TEXTURE_RECTANGLE            = 0x84F5, // 3.1
+      TEXTURE_CUBE_MAP             = 0x8513, // 1.3
+      TEXTURE_1D_ARRAY             = 0x8C18, // 3.0
+      TEXTURE_2D_ARRAY             = 0x8C1A, // 3.0
+      TEXTURE_BUFFER               = 0x8C2A, // 3.1
+      TEXTURE_CUBE_MAP_ARRAY       = 0x9009, // 4.0
+      TEXTURE_2D_MULTISAMPLE       = 0x9100, // 3.2
+      TEXTURE_2D_MULTISAMPLE_ARRAY = 0x9102  // 3.2
+   };
+
+   enum Filter {
+      NEAREST = 0x2600,
+      LINEAR  = 0x2601
+   };
+
+   enum WrapHeuristic {
+      REPEAT          = 0x2901,
+      CLAMP_TO_BORDER = 0x812D,
+      CLAMP_TO_EDGE   = 0x812F,
+      MIRRORED_REPEAT = 0x8370
+   };
+
+   virtual ~Texture();
+   void bind() const;
+   void unbind() const;
+
+   void setMinFilter(Filter filter) const;
+   void setMinFilter(Filter filter, Filter mipmapFilter) const;
+   void setMagFilter(Filter filter) const;
+   void setMaxAnisotropy(int value = -1);
+   void setBorderColor(float r, float g, float b, float a) const;
+
+   static void setActiveTextureImageUnit(int unit);
+   static int getMaxTextureImageUnits();
+   static int getMaxAnisotropyMax();
+
+protected:
+   unsigned int name;
+   Target target;
+
+   explicit Texture(Target target);
+
+private:
+   static int getMaxTextureImageUnitsPriv();
+   static int getMaxAnisotropyMaxPriv();
+};
+
+#endif // TEXTURE_H
